f9005b00d5df5adb1f2922da858ec0c69299c6e6
berka <berkaridha@gmail.com>
2022-12-27 17:15:21 +0700

    Progres

---
+++ RentalSepeda/src/main/java/com/mycompany/pojo/Admin.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pojo;
+
+/**
+ *
+ * @author berka
+ */
+public class Admin {
+    private Integer idAdmin;
+    private String namaAdmin;
+    private Akun akun;
+    private boolean loginStatus;
+
+    public Admin() {
+    }
+
+    public Integer getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(Integer idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNamaAdmin() {
+        return namaAdmin;
+    }
+
+    public void setNamaAdmin(String namaAdmin) {
+        this.namaAdmin = namaAdmin;
+    }
+
+    public Akun getAkun() {
+        return akun;
+    }
+
+    public void setAkun(Akun akun) {
+        this.akun = akun;
+    }
+
+    public boolean isLoginStatus() {
+        return loginStatus;
+    }
+
+    public void setLoginStatus(boolean loginStatus) {
+        this.loginStatus = loginStatus;
+    }
+}
